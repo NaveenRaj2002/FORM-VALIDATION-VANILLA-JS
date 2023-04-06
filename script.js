c694202b14d0ecf55bcbb591b2f20e5c239c723f
Naveen Raj <112478554+NaveenRaj2002@users.noreply.github.com>
2023-04-06 21:51:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const form = document.querySelector("#form");
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const cpassword = document.querySelector("#cpassword");
+
+form.addEventListener("submit", (e) => {
+    if (!validateInputs()){
+        e.preventDefault();
+    }
+    validateInputs();
+})
+
+function validateInputs(){
+    const usernameVal = username.value.trim();
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    let success = true
+
+    if (usernameVal === ''){
+        success = false;
+        setError(username, "Username is required");
+    }
+    else{
+        success = true;
+        setSuccess(username);
+    }
+
+    if (emailVal === ""){
+        success = false;
+        setError(email, "Email is required");
+    }
+    else if (!validateEmail(emailVal)){
+        success = false;
+        setError(email, "Please enter a valid email");
+    }
+    else{
+        success = true;
+        setSuccess(email);
+    }
+
+    if (password === ""){
+        success = false;
+        setError(password, "Password is required");
+    }
+    else if(passwordVal.length < 8){
+        success = false;
+        setError(password, "Password must be atleast 8 characters long");
+    }
+    else{
+        success = true;
+        setSuccess(password);
+    }
+
+    if (cpassword === ""){
+        success = false;
+        setError(password, "Confirm password is required");
+    }
+    else if(cpassword !== passwordVal){
+        success = false;
+        setError(cpassword, "Password does not match");
+    }
+    else{
+        success = true;
+        setSuccess(cpassword);
+    }
+
+    return success;
+}
+
+function setError(element, message){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector(".error");
+
+    errorElement.innerText = message;
+    inputGroup.classList.add("error");
+    inputGroup.classList.remove("success");
+}
+
+function setSuccess(element){
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector(".error");
+
+    errorElement.innerText = "";
+    inputGroup.classList.add("success");
+    inputGroup.classList.remove("error");
+}
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        );
+}
